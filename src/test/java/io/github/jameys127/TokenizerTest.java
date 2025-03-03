95532cf2dbca6e02eac92d8aa82415853c278ed9
Jamey Sheehy <jameys127@gmail.com>
2025-03-03 14:57:20 -0800

    more testing best practices

--- src/test/java/io/github/jameys127/TokenizerTest.java
+++ src/test/java/io/github/jameys127/TokenizerTest.java
@@ -13,6 +13,20 @@ public class TokenizerTest {
     }
 
     @Test
+    public void testSingleWhiteSpace(){
+        final Tokenizer tokenizer = new Tokenizer(" ");
+        tokenizer.skipWhitespace();
+        assertEquals(1, tokenizer.getPosition());
+    }
+    
+    @Test
+    public void testSingleWhiteSpaceAndCharacter(){
+        final Tokenizer tokenizer = new Tokenizer(" 1");
+        tokenizer.skipWhitespace();
+        assertEquals(1, tokenizer.getPosition());
+    }
+
+    @Test
     public void testReadSingleDigitInteger(){
         final Tokenizer tokenizer = new Tokenizer("1");
         assertEquals(Optional.of(new IntegerToken(1)),
@@ -24,4 +38,17 @@ public class TokenizerTest {
         final Tokenizer tokenizer = new Tokenizer("12");
         assertEquals(Optional.of(new IntegerToken(12)), tokenizer.tryReadIntegerToken());
     }
+
+    @Test
+    public void testReadDigitIntergerWithWhiteSpace(){
+        final Tokenizer tokenizer = new Tokenizer("12 ");
+        assertEquals(Optional.of(new IntegerToken(12)), tokenizer.tryReadIntegerToken());
+    }
+
+    @Test
+    public void testReadIntegerWithEmpty(){
+        final Tokenizer tokenizer = new Tokenizer("");
+        assertEquals(Optional.empty(), tokenizer.tryReadIntegerToken());
+    }
+    
 }
